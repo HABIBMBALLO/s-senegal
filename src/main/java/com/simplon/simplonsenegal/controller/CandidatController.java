310f3b21b61f19521042e0c771a36f2039e36a26
habib mballo <mballohabib93@gmail.com>
2022-07-05 09:37:50 +0000

    make service and repository

--- src/main/java/com/simplon/simplonsenegal/controller/CandidatController.java
+++ src/main/java/com/simplon/simplonsenegal/controller/CandidatController.java
@@ -6,12 +6,8 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,10 +38,10 @@ public class CandidatController {
     public List<Candidat> getAllCandidates() {
         return candidatRepository.findAll();
 }
-//    @GetMapping("/candidats/{id}")
-//    public Candidat getCandidatById(int id) {
-//        return candidatRepository.findById(id);
-//    }
+    @GetMapping("/candidats/{id}")
+    public Candidat getCandidatById(int id) {
+        return candidatRepository.findById(id);
+    }
 //    @GetMapping("/candidat/{id}")
 //	public ResponseEntity<Candidat> findById(@PathVariable("id") Long id) {
 //		Optional<Candidat> emp = Optional.  ofNullable(candidatRepository.findById(id));
