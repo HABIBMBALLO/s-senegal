396bf2ba0ebd7bebb5e8a68929615ee9306182ca
habib mballo <mballohabib93@gmail.com>
2022-07-27 15:17:21 +0000

    first commit

--- src/main/java/com/simplon/simplonsenegal/repositories/RoleRepository.java
+++ src/main/java/com/simplon/simplonsenegal/repositories/RoleRepository.java
@@ -1,11 +1,2 @@
-package com.simplon.simplonsenegal.repositories;
-
-import org.apache.catalina.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RestController;
-
-@Repository
-@RestController
-public interface RoleRepository extends JpaRepository<Role, Long> {
+package com.simplon.simplonsenegal.repositories;public interface RoleRepository {
 }
